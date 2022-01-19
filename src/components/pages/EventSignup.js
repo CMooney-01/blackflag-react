537ddf0ae89678ab7c60733dec0b094dc1bff671
unknown <chris.mooney@blackflagbarbell.com.au>
2022-01-19 13:21:58 +1100

    Cleaned up styling on event signup page

--- src/components/pages/EventSignup.js
+++ src/components/pages/EventSignup.js
@@ -17,15 +17,18 @@ class EventSignup extends React.Component {
       return (
           <div className="sign-up">
 
+            <h5>Your are signing up for the following competition:</h5>
 
-          <h3>Your are signing up for the following competition:</h3>
-          <br/>
-          <div className="event-card-body">
-            <div className='event-title'><h4>{this.context.data[compID].event_title}</h4></div>
-            <div className='event-date'><h5>Date of competition: {this.context.data[compID].event_date}</h5></div>
-            <div className='event-desc'><p>{this.context.data[compID].event_description}</p></div>
-            <div className='event-price' id='entryFee'><p>Entry fee: ${this.context.data[compID].event_price}</p></div>
-          </div>
+            <div className="signup-event-container">
+
+              <div className="signup-card-body">
+                <div className='event-title'>{this.context.data[compID].event_title}</div>
+                <div className='event-date NewDateField'>Date of competition: {this.context.data[compID].NewDateField}</div>
+                <div className='event-desc'>{this.context.data[compID].event_description}</div>
+                <div className='event-price' id='entryFee'>Entry fee: ${this.context.data[compID].event_price}</div>
+              </div>
+
+            </div>
 
 
             <div className="stripe-payment-container">
@@ -33,8 +36,6 @@ class EventSignup extends React.Component {
                 <Payment />
               </div>
             </div>
-        
-
 
           </div>
 
