c301d49cf02ba876fe6c176aa72250bdbc3f6edc
unknown <chris.mooney@blackflagbarbell.com.au>
2022-01-19 11:31:42 +1100

    Revised membership page design. Added vector image backgrounds to membership options, edited images to match this design. Also added geomanist font.

--- src/components/pages/Membership.js
+++ src/components/pages/Membership.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import membership1 from './assets/membership1.JPG'
-import membership2 from './assets/membership2.JPG'
-import membership3 from './assets/membership3.JPG'
+import membership1 from './assets/membership1crop.png'
+import membership2 from './assets/membership2crop.png'
+import membership3 from './assets/membership3crop.png'
 
 export default function Membership() {
   return (
@@ -15,11 +15,12 @@ export default function Membership() {
       <div className='membership-option option1'>
         <img className='membership-img' src={membership3} />
         <div className='membership-desc'>
-            <h5>Coaching Membership</h5>
-            <p>
-              Our coaching membership includes unlimited coaching during open hours,
-              access to our conditioning classes,
-              personalised programming and 24/7 access to Black Flag Barbell Club.
+            <h5>COACHING</h5>
+            <p className='membership-para'>
+              For those who want it all! Our most popular membership option includes:<br />
+              Unlimited coaching during staffed hours<br />
+              Personalised programming<br />
+              24/7 access to Black Flag Barbell Club.
             </p>
 
         </div>
@@ -28,15 +29,12 @@ export default function Membership() {
       <div className='membership-option option2'>
         <img className='membership-img' src={membership2} />
         <div className='membership-desc'>
-          <h5>Gym Access Membership</h5>
-          <p>
-            Does exactly what it says on the tin - 24/7 access
+          <h5>GYM ACCESS</h5>
+          <p className='membership-para'>
+            24/7 access
             to Black Flag Barbell Club and it's awesome equipment for powerlifting, strongman,
-            weightlifting and all round strength training.
-          </p>
-          <br />
-          <p>
-            Does not include coaching, programming or access to classes.
+            weightlifting and all round strength training.<br />
+            Does not include coaching or programming.
           </p>
         </div>
       </div>
@@ -44,10 +42,10 @@ export default function Membership() {
       <div className='membership-option option3'>
         <img className='membership-img' src={membership1} />
         <div className='membership-desc'>
-          <h5>Online Coaching</h5>
-          <p>
-            Can't train with us in person? No worries! With weekly check ins, live catch up calls and video reviews
-            each week we will hold you accountable, push you to achieve your best and teach you
+          <h5>ONLINE COACHING</h5>
+          <p className='membership-para'>
+            Can't train with us in person? No worries!<br />
+            Each week we will hold you accountable, push you to achieve your best and teach you
             the 'what', 'why' and 'how' that drives technical and programming decisions.
           </p>
         </div>
