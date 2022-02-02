b1ce6fc2c96ecd13afdd6aa823ef4bc797f8a84d
unknown <chris.mooney@blackflagbarbell.com.au>
2022-02-02 13:51:35 +1100

    Finished (I hope) media query revisions starting from mobile first

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className='home'>
         <div className='container'>
-            <h1>BY LIFTERS</h1> <h1>FOR LIFTERS</h1>
+            <h1 className="home-title">BY LIFTERS</h1> <h1 className="home-title">FOR LIFTERS</h1>
             <div>
               <p>Lifting weights with mates since 2015</p>
             </div>
